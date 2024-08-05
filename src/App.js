adb1fa47115d49fb7ef8bbdbbd1d6d4bff36a672
prakash07naren@gmail.com <prakash07naren@gmail.com>
2024-08-05 18:56:46 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to REACT LEARNING
+          Welcome to REACT LEARNING by PR
         </p>
         <a
           className="App-link"
