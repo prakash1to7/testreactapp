9bacf3bb3d95733aa3c4f7b7e6828ea4564b994a
prakash07naren@gmail.com <prakash07naren@gmail.com>
2024-08-06 10:17:48 +0530

    Test Commit 10:16

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to REACT LEARNING by Self Learning PRAMSAMI
+          Welcome to REACT LEARNING by Self Learning PRAMSAMI10:16
         </p>
         <a
           className="App-link"
